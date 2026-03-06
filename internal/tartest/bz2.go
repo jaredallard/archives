b7694a6bfca1eb740816c2577abb81a25b06d906
Jared Allard <jaredallard@users.noreply.github.com>
2026-03-05 17:04:25 -0800

    chore: lint fix

--- internal/tartest/bz2.go
+++ internal/tartest/bz2.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 archives contributors
+// Copyright (C) 2026 archives contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as
